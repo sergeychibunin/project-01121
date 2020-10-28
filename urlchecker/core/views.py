606e9e36a48c3919c670bf0e87e5325d1a59d1ed
Sergey Chibunin <chibunin1988@gmail.com>
2020-10-28 17:48:33 +0300

    Update the main page.  Add URL's checking

--- urlchecker/core/views.py
+++ urlchecker/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from core.models import Url
 
 
 @login_required(login_url='/accounts/login/')
 def main_page(request):
-    context = {}
+    context = {'urls': Url.objects.all()}
     return render(request, 'index.html', context)
